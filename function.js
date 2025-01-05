8a46707f8f29ed9c32539f716d6866d67356bb5a
Drake Farrokhi <161988303+DrakeFarrokhi@users.noreply.github.com>
2025-01-05 10:20:46 -0700

    Update function.js
    Changing from Kam's Photos to BH Fireplace

--- function.js
+++ function.js
@@ -1,19 +1,1 @@
-let slideIndex = 0;
-showSlides(slideIndex, false);
 
-function showSlides(flag) {
-  let slides = document.querySelectorAll('.slide');
-  if (slideIndex >= slides.length) slideIndex = 0;
-  if (slideIndex < 0) slideIndex = slides.length - 1;
-  for (let i = 0; i < slides.length; i++) {
-    if (i !== slideIndex) slides[i].style.display = 'none';
-    else slides[i].style.display = 'block';
-  }
-  slideIndex++;
-  if (!flag) setTimeout(showSlides, 5000); 
-}
-
-function changeSlide(n) {
-  slideIndex += (n - 1);
-  showSlides(true);
-}
